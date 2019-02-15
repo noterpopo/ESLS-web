9361f3d28299ccc8ccda88fecf3135d0dcfe240f
noterpopo <54336qaz55>
2019-02-15 14:02:02 +0800

    add e-label

---
+++ src/view/single-page/home/e-lable.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>{{itemName}}</div>
+</template>
+
+<script>
+export default {
+  name: 'e_lable',
+  props: {
+    itemName: {
+      type: String,
+      default: '测试商品名称1'
+    },
+    itemUnit: {
+      type: String,
+      default: '盒'
+    },
+    itemNorm: {
+      type: String,
+      default: '250g'
+    },
+    itemOrigin: {
+      type: String,
+      default: '上海'
+    },
+    itemNo: {
+      type: String,
+      default: '00012'
+    },
+    itemQRCode: {
+      type: String,
+      default: '233333'
+    },
+    itemBarCode: {
+      type: String,
+      default: '23333'
+    },
+    itemisOnSale: {
+      type: Boolean,
+      default: false
+    },
+    itemPrice: {
+      type: String,
+      default: '999.99'
+    },
+    itemOnSalePrice: {
+      type: String,
+      default: '999.99'
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
