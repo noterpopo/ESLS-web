f64251d90a8576b3a2eeea14dd9aa75777d32f8c
Noterpopo <popo54336@outlook.com>
2019-02-16 17:45:26 +0800

    加入促销

--- src/view/single-page/home/e-lable.vue
+++ src/view/single-page/home/e-lable.vue
@@ -25,6 +25,10 @@ export default {
       type: String,
       default: '250g'
     },
+    itemCategory: {
+      type: String,
+      default: '食品'
+    },
     itemOrigin: {
       type: String,
       default: '上海'
@@ -59,6 +63,7 @@ export default {
   },
   methods: {
     draw () {
+      this.$Message.info(this.itemName)
       var topMargin = 2
       var leftMargin = 2
       canvas = this.$refs.canvas
@@ -67,42 +72,80 @@ export default {
       canvas.width = 300
       canvas.height = canvas.width / 2
       // 商品名称
-      ctx.font = canvas.width / 15 + 'px 微软雅黑'
+      ctx.font = canvas.width / 12 + 'px 微软雅黑'
       ctx.fillText(this.itemName, leftMargin + 0, topMargin + canvas.width / 15)
       // 下划线
       ctx.beginPath()
       ctx.moveTo(leftMargin + 0, topMargin + canvas.width / 15 + 10)
       ctx.lineTo(canvas.width, topMargin + canvas.width / 15 + 10)
       ctx.stroke()
-      // 价格整
-      ctx.font = canvas.width / 6 + 'px 微软雅黑'
-      ctx.textBaseline = 'hanging'
-      ctx.fillText(this.itemPrice.split('.')[0] + '.', leftMargin + 8, topMargin + canvas.width / 6)
-      // 价格小
-      ctx.font = canvas.width / 7.5 + 'px 微软雅黑'
-      ctx.fillText(this.itemPrice.split('.')[1], leftMargin + 8 + canvas.width / 9 * this.itemPrice.split('.')[0].length, topMargin + canvas.width / 6)
+      console.log(this.itemisOnSale)
+      if (this.itemisOnSale) {
+        ctx.textBaseline = 'middle'
+        // 原价整
+        ctx.font = 'bold ' + canvas.width / 16 + 'px 微软雅黑'
+        ctx.fillText('原价：￥' + this.itemPrice.split('.')[0] + '.', leftMargin + 8, topMargin + canvas.width / 15 + 10 + canvas.width / 16)
+        // 原价小
+        ctx.font = 'bold ' + canvas.width / 18 + 'px 微软雅黑'
+        ctx.fillText(this.itemPrice.split('.')[1], leftMargin + 8 + canvas.width / 19.5 * (this.itemPrice.split('.')[0].length + 4) + 10, topMargin + canvas.width / 15 + 10 + canvas.width / 16 * 0.9)
+        // 划线
+        ctx.beginPath()
+        ctx.moveTo(leftMargin + 8 + canvas.width / 19.5 * 4, topMargin + canvas.width / 15 + 10 + canvas.width / 16)
+        ctx.lineTo(leftMargin + 8 + canvas.width / 19.5 * (this.itemPrice.split('.')[0].length + 4) + 10, topMargin + canvas.width / 15 + 10 + canvas.width / 16)
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.moveTo(leftMargin + 8 + canvas.width / 19.5 * (this.itemPrice.split('.')[0].length + 4) + 10, topMargin + canvas.width / 15 + 10 + canvas.width / 16 * 0.9)
+        ctx.lineTo(leftMargin + 8 + canvas.width / 19.5 * (this.itemPrice.split('.')[0].length + 5) + 10, topMargin + canvas.width / 15 + 10 + canvas.width / 16 * 0.9)
+        ctx.stroke()
+        // 红色底
+        ctx.fillStyle = 'red'
+        ctx.fillRect(leftMargin + 8 + 3 * canvas.width / 16, topMargin + canvas.width / 15 + 10 + canvas.width / 16 + canvas.width / 18, canvas.width / 13 * 5, canvas.width / 8)
+        // 促价整
+        ctx.font = 'bold ' + canvas.width / 16 + 'px 微软雅黑'
+        ctx.fillText('现价：', leftMargin + 8, topMargin + canvas.width / 15 + 10 + canvas.width / 16 + canvas.width / 18 + canvas.width / 15)
+        ctx.fillStyle = 'white'
+        ctx.font = 'bold ' + canvas.width / 12 + 'px 微软雅黑'
+        ctx.fillText('￥' + this.itemOnSalePrice.split('.')[0] + '.', leftMargin + 8 + 3 * canvas.width / 16, topMargin + canvas.width / 15 + 10 + canvas.width / 16 + canvas.width / 18 + canvas.width / 15)
+        // 促价小
+        ctx.font = 'bold ' + canvas.width / 14 + 'px 微软雅黑'
+        ctx.fillText(this.itemOnSalePrice.split('.')[1], leftMargin + 8 + canvas.width / 16.5 * (this.itemOnSalePrice.split('.')[0].length + 4) + 10, topMargin + canvas.width / 15 + 10 + canvas.width / 16 + canvas.width / 18 * 0.8 + canvas.width / 15)
+        ctx.textBaseline = 'hanging'
+      } else {
+        ctx.textBaseline = 'hanging'
+        // 价格整
+        ctx.font = canvas.width / 5 + 'px 微软雅黑'
+        ctx.fillText(this.itemPrice.split('.')[0] + '.', leftMargin + 8, topMargin + canvas.width / 6)
+        // 价格小
+        ctx.font = canvas.width / 6.5 + 'px 微软雅黑'
+        ctx.fillText(this.itemPrice.split('.')[1], leftMargin + 8 + canvas.width / 7.5 * this.itemPrice.split('.')[0].length, topMargin + canvas.width / 6)
+      }
+      ctx.fillStyle = 'black'
       // 右边框信息-规格
       ctx.font = canvas.width / 18.75 + 'px 微软雅黑'
-      ctx.fillText('规格：' + this.itemNorm, leftMargin + 8 + canvas.width / 9 * this.itemPrice.split('.')[0].length + this.itemPrice.split('.')[1].length * canvas.width / 12.5 + 20, topMargin + canvas.width / 6)
+      ctx.fillText('规格：' + this.itemNorm, canvas.width * 0.68, topMargin + canvas.width / 6)
       // 右边框信息-类别
       ctx.font = canvas.width / 18.75 + 'px 微软雅黑'
-      ctx.fillText('类别：' + this.itemQRCode, leftMargin + 8 + canvas.width / 9 * this.itemPrice.split('.')[0].length + this.itemPrice.split('.')[1].length * canvas.width / 12.5 + 20, topMargin + canvas.width / 6 + canvas.width / 13.5 * 1)
+      ctx.fillText('类别：' + this.itemCategory, canvas.width * 0.68, topMargin + canvas.width / 6 + canvas.width / 13.5 * 1)
       // 右边框信息-单位
       ctx.font = canvas.width / 18.75 + 'px 微软雅黑'
-      ctx.fillText('单位：' + this.itemUnit, leftMargin + 8 + canvas.width / 9 * this.itemPrice.split('.')[0].length + this.itemPrice.split('.')[1].length * canvas.width / 12.5 + 20, topMargin + canvas.width / 6 + canvas.width / 13.5 * 2)
+      ctx.fillText('单位：' + this.itemUnit, canvas.width * 0.68, topMargin + canvas.width / 6 + canvas.width / 13.5 * 2)
       // 右边框信息-产地
       ctx.font = canvas.width / 18.75 + 'px 微软雅黑'
-      ctx.fillText('产地：' + this.itemOrigin, leftMargin + 8 + canvas.width / 9 * this.itemPrice.split('.')[0].length + this.itemPrice.split('.')[1].length * canvas.width / 12.5 + 20, topMargin + canvas.width / 6 + canvas.width / 13.5 * 3)
+      ctx.fillText('产地：' + this.itemOrigin, canvas.width * 0.68, topMargin + canvas.width / 6 + canvas.width / 13.5 * 3)
       // 条形码
       var barcode = document.getElementById('barcode')
       barcode.width = barcode.width
-      var barcodewidth = canvas.width / 2.5
-      var barcodeheight = canvas.height / 4
+      var barcodewidth = canvas.width / 2.2
+      var barcodeheight = canvas.height / 5
       JsBarcode('#barcode', this.itemBarCode, {
-        fontSize: canvas.width / 6
+        format: 'EAN13',
+        fontSize: canvas.width / 6,
+        margin: 0,
+        textMargin: 0,
+        width: this.itemBarCode.length / 3
       })
       var barcodeimg = convertCanvasToImage(barcode)
-      barcodeimg.onload = function () { ctx.drawImage(barcodeimg, leftMargin, canvas.height * 0.74, barcodewidth, barcodeheight) }
+      barcodeimg.onload = function () { ctx.drawImage(barcodeimg, leftMargin, canvas.height * 0.76, barcodewidth, barcodeheight) }
     }
   }
 }
--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <e_label v-bind="item" ref="label_canvas" ></e_label>
-    <input type="text" v-model="item.itemName" @change="onItemChange"/>
-    <input type="text" v-model="item.itemPrice" @change="onItemChange"/>
+    <input type="text" v-model="item.itemName" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemUnit" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemNorm" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemCategory" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemOrigin" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemNo" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemQRCode" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemBarCode" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemPrice" @on-change="onItemChange"/>
+    <Input type="text" v-model="item.itemOnSalePrice" @on-change="onItemChange"/>
+    <i-switch v-model="item.itemisOnSale" @on-change="onItemChange" />
+    <ul id="v-for-object" class="demo">
+</ul>
+
   </div>
 </template>
 
@@ -19,14 +31,14 @@ export default {
         itemName: '测试商品名称1',
         itemUnit: '盒',
         itemNorm: '250g',
+        itemCategory: '食品',
         itemOrigin: '上海',
         itemNo: '00012',
         itemQRCode: '233333',
-        itemBarCode: '233333',
+        itemBarCode: '692226641428',
         itemisOnSale: false,
         itemPrice: '999.99',
         itemOnSalePrice: '999.99'
-
       }
     }
   },
