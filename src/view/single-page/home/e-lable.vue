f32137486305839d0123d199cf4e655104681b0f
Noterpopo <popo54336@outlook.com>
2019-02-17 22:05:44 +0800

    去除重复baseline

--- src/view/single-page/home/e-lable.vue
+++ src/view/single-page/home/e-lable.vue
@@ -130,100 +130,71 @@ export default {
         widthRadius = canvas.width / 250
         heightRadius = canvas.height / 122
         // 商品名称
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('', 2 * widthRadius, 18 * heightRadius, 0, 0, 1, this.itemName, '', '', 'normal', '微软雅黑', 0, 21 * widthRadius)
         // 下划线
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('线段', 2 * widthRadius, 28 * heightRadius, 250 * widthRadius, 0, 1, '', '', '', 'normal', '微软雅黑', 1, 0)
         if (this.itemisOnSale) {
           // 原价整
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 10 * widthRadius, 48 * heightRadius, 0, 0, 1, '原价：￥', '', '', 'bold', '微软雅黑', 0, 16 * widthRadius)
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 72 * widthRadius, 48 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[0], '', '.', 'bold', '微软雅黑', 0, 16 * widthRadius)
           // 原价小
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 80 * widthRadius, 46 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[1], '', '', 'bold', '微软雅黑', 0, 14 * widthRadius, this.itemPrice.split('.')[0], 8 * widthRadius)
           // 划线
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('线段', 68 * widthRadius, 42 * heightRadius, 0, 0, 1, '', '', '', 'normal', '微软雅黑', 1, 0, '', 0, this.itemPrice.split('.')[0], 14 * widthRadius)
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('线段', 80 * widthRadius, 40 * heightRadius, 0, 0, 1, '', '', '', 'normal', '微软雅黑', 1, 0, this.itemPrice.split('.')[0], 8 * widthRadius, this.itemPrice.split('.')[1], 10 * widthRadius)
           // 红色底
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('背景', 57 * widthRadius, 56 * heightRadius, 96 * widthRadius, 32 * heightRadius, 2, '', '', '', 'bold', '微软雅黑', 2, 16 * widthRadius)
           // 促价整
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 10 * widthRadius, 80 * heightRadius, 0, 0, 1, '现价：', '', '', 'bold', '微软雅黑', 2, 16 * widthRadius)
           ctx.fillStyle = 'white'
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 57 * widthRadius, 80 * heightRadius, 0, 0, 1, this.itemOnSalePrice.split('.')[0], '￥', '.', 'bold', '微软雅黑', 1, 21 * widthRadius)
           // 促价小
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 88 * widthRadius, 77 * heightRadius, 0, 0, 1, this.itemOnSalePrice.split('.')[1], '', '', 'bold', '微软雅黑', 1, 18 * widthRadius, this.itemOnSalePrice.split('.')[0], 12 * widthRadius)
         } else {
           // 价格整
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 10 * widthRadius, 86 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[0], '', '.', 'normal', '微软雅黑', 0, 50 * widthRadius)
           // 价格小
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 10 * widthRadius, 78 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[1], '', '', 'normal', '微软雅黑', 0, 38 * widthRadius, this.itemPrice.split('.')[0], 33 * widthRadius)
         }
         // 右边框信息-规格
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('', 170 * widthRadius, 56 * heightRadius, 0, 0, 1, this.itemNorm, '规格：', '', 'normal', '微软雅黑', 0, 13 * widthRadius)
         // 右边框信息-类别
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('', 170 * widthRadius, 74 * heightRadius, 0, 0, 1, this.itemCategory, '类别：', '', 'normal', '微软雅黑', 0, 13 * widthRadius)
         // 右边框信息-单位
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('', 170 * widthRadius, 92 * heightRadius, 0, 0, 1, this.itemUnit, '单位：', '', 'normal', '微软雅黑', 0, 13 * widthRadius)
         // 右边框信息-产地
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('', 170 * widthRadius, 110 * heightRadius, 0, 0, 1, this.itemOrigin, '产地：', '', 'normal', '微软雅黑', 0, 13 * widthRadius)
         // 条形码
         this.drawDispms('条形码', 2 * widthRadius, 96 * heightRadius, 114 * widthRadius, 25 * heightRadius, 0, this.itemBarCode, '', '', 0, '', '', 67 * widthRadius)
       } else if (this.labelStyle === '2') {
-        ctx.textBaseline = 'alphabetic'
         canvas.width = 400
         canvas.height = 300
         widthRadius = canvas.width / 400
         heightRadius = canvas.height / 300
         // 商品名称
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('', 2 * widthRadius, 30 * heightRadius, 0, 0, 1, this.itemName, '', '', 'normal', '微软雅黑', 0, 33 * widthRadius)
         // 下划线
-        ctx.textBaseline = 'alphabetic'
         this.drawDispms('线段', 2 * widthRadius, 38 * heightRadius, 400 * widthRadius, 0, 1, '原价：￥', '', '', 'normal', '微软雅黑', 0, 20 * widthRadius)
-
         if (this.itemisOnSale) {
           // '原价：￥'
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 6 * widthRadius, 76 * heightRadius, 0, 0, 1, '原价：￥', '', '', 'normal', '微软雅黑', 0, 20 * widthRadius)
           // 原价整
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 86 * widthRadius, 76 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[0], '', '.', 'oblique', '微软雅黑', 0, 20 * widthRadius)
           // 原价小
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 86 * widthRadius, 72 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[1], '', '', 'oblique', '微软雅黑', 0, 15 * widthRadius, this.itemPrice.split('.')[0], 14 * widthRadius)
           // 划线
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('线段', 86 * widthRadius, 70 * heightRadius, 0, 0, 1, '', '', '', 'normal', '微软雅黑', 2, 0, '', 0, this.itemPrice.split('.')[0], 14 * widthRadius)
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('线段', 86 * widthRadius, 67 * heightRadius, 0, 0, 1, '', '', '', 'normal', '微软雅黑', 2, 0, this.itemPrice.split('.')[0], 14 * widthRadius, this.itemPrice.split('.')[1], 12 * widthRadius)
           ctx.fillStyle = 'red'
           // '现价：￥'
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 6 * widthRadius, 150 * heightRadius, 0, 0, 1, '现价：￥', '', '', 'normal', '微软雅黑', 2, 20 * widthRadius)
           // 现价整
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 82 * widthRadius, 150 * heightRadius, 0, 0, 1, this.itemOnSalePrice.split('.')[0], '', '.', 'bold', '微软雅黑', 2, 66 * widthRadius)
           // 现价小
           this.drawDispms('', 82 * widthRadius, 140 * heightRadius, 0, 0, 1, this.itemOnSalePrice.split('.')[1], '', '', 'bold', '微软雅黑', 2, 57 * widthRadius, this.itemOnSalePrice.split('.')[0], 48 * widthRadius)
           ctx.fillStyle = 'black'
         } else {
           // '￥'号
-          ctx.textBaseline = 'alphabetic'
           this.drawDispms('', 6 * widthRadius, 110 * heightRadius, 0, 0, 1, '￥', '', '', 'normal', '微软雅黑', 0, 44 * widthRadius)
           // 价格整
           this.drawDispms('', 48 * widthRadius, 140 * heightRadius, 0, 0, 1, this.itemPrice.split('.')[0], '', '.', 'normal', '微软雅黑', 0, 80 * widthRadius)
@@ -232,7 +203,6 @@ export default {
           // '元'
           this.drawDispms('', 64 * widthRadius, 150 * heightRadius, 0, 0, 1, '元', '', '', 'normal', '微软雅黑', 0, 44 * widthRadius, this.itemPrice.split('.')[0], 50 * widthRadius, this.itemPrice.split('.')[1], 40 * widthRadius)
         }
-        ctx.textBaseline = 'alphabetic'
         // 规格
         this.drawDispms('', 6 * widthRadius, 204 * heightRadius, 0, 0, 1, this.itemNorm, '规格：', '', 'normal', '微软雅黑', 0, 17 * widthRadius)
         // 单位
