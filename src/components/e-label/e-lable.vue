13cddff46862e58dcf88ccac6b18627e682ff345
noterpopo <popo54336@outlook.com>
2019-03-25 16:43:02 +0800

    add top align

--- src/components/e-label/e-lable.vue
+++ src/components/e-label/e-lable.vue
@@ -2,14 +2,14 @@
     <div class ="label-canvas">
       <slot></slot>
       <canvas id="tcanvas" ref="canvas"></canvas>
-      <div class="right">
+      <div v-if="isShow" class="right">
           <div class="editorarea" :style="{width:styleWidth+'px',height:styleHeight+'px'}">
             <div v-for="(item,index) in dispmsData" :key="index" v-show="item.status===1" :style="{position:'absolute', left:item.x+'px', top:item.y+'px'}">
               <div :style="{textAlign:'left',position: 'absolute',left: 0+'px',top: 0+'px',width:item.width+'px',height:item.height+'px',backgroundColor:item.backgroundColor==='0'?'black':item.backgroundColor==='1'?'white':'red'}">
                 <span v-if="item.columnType === '字符串'" :style="{ color:item.fontColor==='0'?'black':item.fontColor==='1'?'white':'red', fontSize :item.fontSize+'px', fontWeight:item.fontType, lineHeight:item.height+'px', fontFamily:item.fontFamily, fontStyle:item.fontType}">{{item.startText + item.text + item.endText}}</span>
                 <span v-else-if="item.columnType === '数字'" >
                   <span :class="item.backup.split('/')[0]==='1' ? 'line' : '' " :style="{ color:item.fontColor==='0'?'black':item.fontColor==='1'?'white':'red', fontSize :item.fontSize+'px', fontWeight:item.fontType,  fontFamily:item.fontFamily, fontStyle:item.fontType}">{{item.text.split('.')[0] +'.'}}</span>
-                  <span :class="item.backup.split('/')[0]==='1' ? 'line' : '' " :style="{ color:item.fontColor==='0'?'black':item.fontColor==='1'?'white':'red', verticalAlign:'super',fontSize :(item.sourceColumn==='promotePrice'?decFontSizePromotePrice:decFontSizePrice)+'px', fontWeight:item.fontType,  fontFamily:item.fontFamily, fontStyle:item.fontType}">{{ item.text.split('.')[1]}}</span>
+                  <span :class="item.backup.split('/')[0]==='1' ? 'line' : '' " :style="{ color:item.fontColor==='0'?'black':item.fontColor==='1'?'white':'red', verticalAlign:'top',fontSize :(item.sourceColumn==='promotePrice'?decFontSizePromotePrice:decFontSizePrice)+'px', fontWeight:item.fontType,  fontFamily:item.fontFamily, fontStyle:item.fontType}">{{ item.text.split('.')[1]}}</span>
                 </span>
                 <hr v-else-if="item.columnType === '线段'" class="hrline" :style="{height:item.backup+'px',background : '#000'}">
                 <img v-else-if="item.columnType === '二维码'" id="tqrCodeImg" :style="{ width:item.width+'px', height:item.height+'px'}"/>
@@ -83,6 +83,7 @@ export default {
   },
   data () {
     return {
+      isShow: false,
       styleWidth: 0,
       styleHeight: 0,
       dispmsData: [],
@@ -141,10 +142,12 @@ export default {
   methods: {
     initData (data, width, height) {
       if (data === null) {
+        this.dispmsData = []
         this.styleWidth = 0
         this.styleHeight = 0
         return
       }
+      this.isShow = true
       var len = data.length
       // 循环变量
       for (var i = 0; i < len; ++i) {
