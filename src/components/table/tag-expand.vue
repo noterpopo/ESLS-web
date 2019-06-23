60ab014c40a2eb969839f67b0f2fa25f77c651b2
noterpopo <popo54336@outlook.com>
2019-06-23 11:52:38 +0800

    fix

--- src/components/table/tag-expand.vue
+++ src/components/table/tag-expand.vue
@@ -14,12 +14,12 @@
                 <span style="margin-left:4px" class="expand-key">软件版本： </span>
                 <span class="expand-value">{{ row.softwareVersion }}</span>
             </Col>
-            <Col span="8">
+            <!-- <Col span="8">
                 <span style="margin-left:4px" class="expand-key">总重量： </span>
                 <span class="expand-value">{{ row.totalWeight }}</span>
-            </Col>
+            </Col> -->
         </Row>
-        <Row>
+        <Row class="expand-row">
             <Col span="8">
                 <span style="margin-left:4px" class="expand-key">运行时间： </span>
                 <span class="expand-value">{{ row.execTime +' ms' }}</span>
@@ -27,6 +27,20 @@
             <Col span="8">
                 <span style="margin-left:4px" class="expand-key">完成时间：</span>
                 <span class="expand-value">{{ row.completeTime }}</span>
+            </Col>
+            <!-- <Col span="8">
+                <span style="margin-left:4px" class="expand-key">电子称电量： </span>
+                <span class="expand-value">{{ row.measurePower }}</span>
+            </Col> -->
+        </Row>
+        <Row>
+            <Col span="8">
+                <span style="margin-left:4px" class="expand-key">总重量： </span>
+                <span class="expand-value">{{ row.totalWeight }}</span>
+            </Col>
+            <Col span="8">
+                <span style="margin-left:4px" class="expand-key">商品个数：</span>
+                <span class="expand-value">{{ row.goodNumber }}</span>
             </Col>
             <Col span="8">
                 <span style="margin-left:4px" class="expand-key">电子称电量： </span>
