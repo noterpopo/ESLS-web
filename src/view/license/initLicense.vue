4f4c0dc3ae962ebcefdb5d94e0ca6614dd7751fa
noterpopo <popo54336@outlook.com>
2019-08-24 14:01:55 +0800

    baseurl

--- src/view/license/initLicense.vue
+++ src/view/license/initLicense.vue
@@ -1,16 +1,30 @@
 <template>
     <div class="initLicense" ref="container" style="display: flex;flex-direction: column;flex-wrap: wrap;justify-content: flex-start; align-items: center;align-content: center;">
-        <Card class="card" title="注册证书" :bordered="false" v-bind:style="{ width: windowWidth*0.6 + 'px' }">
-
+        <Card class="card" title="注册证书" :bordered="false" v-bind:style="{ width: windowWidth*0.2 + 'px' }">
+          <Button type="primary" @click="showServerInfo">查看服务器信息</Button>
         </Card>
+        <Modal v-model="isShow" title="服务器信息">
+              <div class="font">
+                <p>{{'CPU：'+cpuSerial}}</p>
+                <p>{{'IP地址：'+ipAddress}}</p>
+                <p>{{'MAC地址：'+macAddress}}</p>
+                <p>{{'主板：'+mainBoardSerial}}</p>
+              </div>
+            </Modal>
     </div>
 </template>
 
 <script>
+import { getServerInfos } from '@/api/license'
 export default {
   data () {
     return {
-      windowWidth: 0
+      windowWidth: 0,
+      isShow: false,
+      cpuSerial: '',
+      ipAddress: '',
+      macAddress: '',
+      mainBoardSerial: ''
     }
   },
   mounted () {
@@ -23,7 +37,16 @@ export default {
     }
   },
   methods: {
+    showServerInfo () {
+      getServerInfos('ubuntu').then(res => {
+        this.cpuSerial = res.data.cpuSerial
+        this.ipAddress = res.data.ipAddress
+        this.macAddress = res.data.macAddress
+        this.mainBoardSerial = res.data.mainBoardSerial
 
+        this.isShow = true
+      })
+    }
   }
 }
 </script>
@@ -39,5 +62,8 @@ export default {
       margin-top: 80px;
       display: inline-block;
     }
+}
+.font{
+  font-size: 16px;
 }
 </style>
