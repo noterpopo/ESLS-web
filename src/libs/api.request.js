a034718020c3b723e869b649338686b01a2ad480
梁仕钢 <popo@liangshigangs-MacBook-Air.local>
2019-10-15 20:07:05 +0800

    动态url

--- src/libs/api.request.js
+++ src/libs/api.request.js
@@ -1,6 +1,13 @@
 import HttpRequest from '@/libs/axios'
 import config from '@/config'
-const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
 
+function getBaseUrl () {
+  config.baseUrl.dev = window.location.host.split(':')[0] + ':8086'
+  config.baseUrl.pro = window.location.host.split(':')[0] + ':8086'
+}
+getBaseUrl()
+console.log(config.baseUrl.dev)
+
+const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro
 const axios = new HttpRequest(baseUrl)
 export default axios
