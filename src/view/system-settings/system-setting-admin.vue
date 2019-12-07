dd2b9ac83c88ae60b5e1fbc1c674014e49cd2f70
noterpopo <popo54336@outlook.com>
2019-12-07 22:05:13 +0800

    ceshi

--- src/view/system-settings/system-setting-admin.vue
+++ src/view/system-settings/system-setting-admin.vue
@@ -108,6 +108,7 @@ export default {
         'spec',
         'stock',
         'unit',
+        'shopNumber',
         'shelfNumber',
         'promoteTimeGap',
         'promotionReason',
@@ -146,7 +147,8 @@ export default {
         weightSpec: '重量规格',
         replenishNumber: '预警数量',
         barCode: '商品条码',
-        computeNumber: '件数'
+        computeNumber: '件数',
+        shopNumber: '店铺编号'
       }
     }
   },
